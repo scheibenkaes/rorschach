0975366587dcab4c0a7839ae8e1681e194873f0f
Benjamin Klüglein <scheibenkaes@googlemail.com>
2013-01-28 20:59:51 +0100

    added doc string

--- src/rorschach/core.clj
+++ src/rorschach/core.clj
@@ -1,6 +1,10 @@
 (ns rorschach.core)
 
-(defmacro derive-atom [atm new-atm key f & [inital-value]]
+(defmacro derive-atom
+  "Defines a new atom 'new-atm' and adds a watch to the original atom.
+ whenever the origin gets updated f will be called with the new value
+ and its return value will be stored into new-atm."
+  [atm new-atm key f & [inital-value]]
   `(do
      (def ~new-atm (atom (or ~inital-value (~f (deref ~atm)))))
      (add-watch ~atm ~key (fn [r# k# old# n#]
