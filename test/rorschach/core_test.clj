0e11a712873201785fdd47ead0bd922454559919
Benjamin Klüglein <scheibenkaes@googlemail.com>
2013-01-28 21:29:43 +0100

    added tests

--- test/rorschach/core_test.clj
+++ test/rorschach/core_test.clj
@@ -2,6 +2,13 @@
   (:use clojure.test
         rorschach.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def a (atom 1))
+
+(testing "definition of the new atm"
+  (derive-atom a b :test inc)
+  (is (= 2 @b)))
+
+(testing "updating the source changes the new-atm"
+  (derive-atom a b' :test inc)
+  (reset! a 2)
+  (is (= 3 @b')))
