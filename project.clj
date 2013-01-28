91396b057b9fee6e9ee08bfcb02d40592b9c3a61
Benjamin Klüglein <scheibenkaes@googlemail.com>
2013-01-28 21:36:10 +0100

    updated project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject rorschach "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject org.scheibenkaes/rorschach "0.1.0"
+  :description "Lib to watch stuff and react on it."
+  :url "http://scheibenkaes.org"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
